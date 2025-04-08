270923c499578ac1d56a328d9efc822463dc4a40
victortelles <victor.telles@iteso.mx>
2025-04-08 15:16:12 -0600

    widget de actualizar

--- lib/widgets/button_update.dart
+++ lib/widgets/button_update.dart
@@ -1,12 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-//Funcion update
-/*
-Future<void> updateProduct(String productId, String newName){
-  CollectionReference products = FirebaseFirestore.instance.collection('products');
+class BottonUpdate extends StatelessWidget {
+  final String productId;
+  final String newName;
 
-  return products.doc(productId).update({'name': newName})
-    .then((value) => print('Product name updated successfully'))
-    .catchError((error) => print('Failed to update product name: $error'));
+  const BottonUpdate({
+    super.key,
+    required this.productId,
+    required this.newName,
+  });
+
+
+  //Funcion update
+  Future<void> updateProduct(String productId, String newName){
+    CollectionReference products = FirebaseFirestore.instance.collection('products');
+
+    return products.doc(productId).update({'name': newName})
+      .then((value) => print('Product name updated successfully'))
+      .catchError((error) => print('Failed to update product name: $error'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.edit, color: Colors.blue),
+      tooltip: "Editar",
+      onPressed: () async {
+        await updateProduct(productId, newName);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Producto actualizado con éxito.')),
+        );
+      },
+    );
+  }
 }
 
-*/
